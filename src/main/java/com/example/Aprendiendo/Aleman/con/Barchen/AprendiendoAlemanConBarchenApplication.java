c3dcd9fc443e1192b40f37f2c9b89210864febb5
Camilo Fernandez <camilo.fernandez@sapiensdev.com>
2025-04-28 21:11:55 -0600

    first commit

---
+++ src/main/java/com/example/Aprendiendo/Aleman/con/Barchen/AprendiendoAlemanConBarchenApplication.java
@@ -0,0 +1,13 @@
+package com.example.Aprendiendo.Aleman.con.Barchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendiendoAlemanConBarchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendiendoAlemanConBarchenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Aprendiendo/Aleman/con/Barchen/AprendiendoAlemanConBarchenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Aprendiendo.Aleman.con.Barchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprendiendoAlemanConBarchenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
